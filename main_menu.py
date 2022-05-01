2bf0ee9e8f8a2f59213f241f67b684f5ed969926
JasminSK <jasminkufner999@gmail.com>
2022-05-01 13:33:11 +0200

    Added function to navigate menu
    - enables to select 'Play Game'
    Added function to navigate menu
    
    - enables to select 'Play Game'

---
+++ main_menu.py
@@ -0,0 +1,19 @@
+from pick import pick
+
+
+# insert following code into Menu class
+def navigate_menu():
+    """
+    Makes navigation of menu possible
+    :return: chosen menu option (starts with 1)
+    """
+    options = ['[Play Game]', '[Quit]']
+    option, index = pick(options, indicator='> ')
+    # insert function for quit button
+    print(' You have chosen ' + option)
+    return index+1
+
+
+if __name__ == "__main__":
+    print(navigate_menu())
+
