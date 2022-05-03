6d0f24b4bc7a40429366ad4a06655aa1f5646440
moritz <contact@moritzenderle.com>
2022-05-03 15:12:36 +0200

    piconneeeect

--- artificial_intelligence.py
+++ artificial_intelligence.py
@@ -2,6 +2,10 @@ from game_elements import Player, GameBoard
 
 
 class AIPlayer(Player):
+    """
+    piconeeeeeeeeeeeeeect
+    """
+
     def __init__(self, player_id, game_board: GameBoard, checkers: int = 21):
         super().__init__(player_id, game_board, checkers)
 
